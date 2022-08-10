c45533601c75a9b90c037b52698911aa11ebcda6
huntmori <huntmori04x22@gmail.com>
2022-08-10 23:55:46 +0900

    add jpa

---
+++ src/main/java/com/demo/app/api/chat/document/TestEntity.java
@@ -0,0 +1,14 @@
+package com.demo.app.api.chat.document;
+
+import org.springframework.data.util.Version;
+import sun.text.normalizer.VersionInfo;
+
+
+public class TestEntity {
+    private VersionInfo version;
+    private Version version2;
+
+    public void test(){
+        
+    }
+}
