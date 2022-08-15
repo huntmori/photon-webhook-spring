33406f86bdb278a3640f511d24450f30caffa6ab
huntmori <huntmori04x22@gmail.com>
2022-08-16 02:32:44 +0900

    update document - chatPrivateMessage - chatPublicMessage

--- src/main/java/com/demo/app/api/chat/document/ChatPrivateMessage.java
+++ src/main/java/com/demo/app/api/chat/document/ChatPrivateMessage.java
@@ -4,11 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.bson.json.JsonObject;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatPrivateMessage {
+    @MongoId
+    private ObjectId _id;
+    private ObjectId chatChannelId;
+    private ObjectId chatUserId;
+    private ObjectId receiveUserId;
 
+    private JsonObject message;
+
+    private List<String> usersPair = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/demo/app/api/chat/document/ChatPublicMessage.java
+++ src/main/java/com/demo/app/api/chat/document/ChatPublicMessage.java
@@ -4,11 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.bson.json.JsonObject;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatPublicMessage {
+    @MongoId
+    private ObjectId _id;
+    private ObjectId chatChannelId;
+    private ObjectId chatUserId;
 
+    private String appVersion;
+
+    private JsonObject message;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
