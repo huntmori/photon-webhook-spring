e5c6762a8f680ffa9597cbb178c38cab65f2e25c
huntmori <huntmori04x22@gmail.com>
2022-08-16 02:27:27 +0900

    update chatUser Document

--- src/main/java/com/demo/app/api/chat/document/ChatUser.java
+++ src/main/java/com/demo/app/api/chat/document/ChatUser.java
@@ -1,14 +1,30 @@
 package com.demo.app.api.chat.document;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Getter
+@Setter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Document
 @ToString
 public class ChatUser {
+    @MongoId
+    @Id
+    private ObjectId _id;
 
+    private String chatUserId;
+    private String platformId;
+
+    private String lastAppVersion;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
